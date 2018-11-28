f291f05e2f74a8dbe134a6795f815ef5815fdf57
Jerko Steiner <jerko.steiner@nyumc.org>
2018-11-28 21:45:11 +0100

    Add ability to search for custom character sets

--- whois.py
+++ whois.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from functools import reduce
 import argparse
 import itertools
 import socket
@@ -8,7 +9,7 @@ HOST='whois.crsnic.net'
 PORT=43
 
 
-class WhoIs:
+class Whois:
     def __init__(self, server, port):
         self.server = server
         self.port = port
@@ -20,7 +21,11 @@ class WhoIs:
         sock.connect((self.server, self.port))
         sock.send(("%s\r\n" % domain).encode("utf-8"))
 
-        value = self._recv(sock)
+        try:
+            value = self._recv(sock)
+        finally:
+            # sock.shutdown(socket.SHUT_RDWR)
+            sock.close()
         if 'No match for' in value:
             return False
         return True
@@ -45,13 +50,29 @@ class Wildcard:
     def __init__(self, wildcard):
         self.wildcard = wildcard
 
-    @property
-    def values(self):
-        wildcards = []
-        letters = []
+        self.wildcards = wildcards = []
+        self.letters = letters = []
 
-        for c in self.wildcard:
-            if c == 'A':
+        bracket_open = False
+        tmp = None
+        for i, c in enumerate(self.wildcard):
+            if c == '[':
+                self._assert(
+                    not bracket_open,
+                    'Wildcard compile error at: {}'.format(i))
+                bracket_open = True
+                tmp = ''
+            elif c == ']':
+                self._assert(
+                    bracket_open,
+                    'Closing brace: {}'.format(i))
+                bracket_open = False
+                wildcards.append(tmp)
+                tmp = None
+                letters.append('_')
+            elif bracket_open:
+                tmp += c
+            elif c == 'A':
                 wildcards.append(alphabet)
                 letters.append('_')
             elif c == 'C':
@@ -68,6 +89,19 @@ class Wildcard:
                 letters.append('_')
             else:
                 letters.append(c)
+
+        if bracket_open:
+            raise ValueError('Wildcard compile error: no closing bracket')
+        self.combinations = reduce(lambda x, y: x * len(y), wildcards, 1)
+
+    def _assert(self, value, message):
+        if not value:
+            raise ValueError(message)
+
+    @property
+    def values(self):
+        wildcards = self.wildcards
+        letters = self.letters
 
         if len(wildcards) == 0:
             yield self.wildcard
@@ -88,12 +122,20 @@ class Wildcard:
 
 
 def parse_args(argv):
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description='Search for unregistered domains. Wildcards supported are:'
+        '\n  A: alphabet characters'
+        '\n  C: consonants'
+        '\n  V: vowels'
+        '\n  #: numbers 0-9'
+        '\n  *: combination of all of the above'
+        '\n  [chars]: chars in the brackets',
+        formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument('--host', default=HOST,
-                        help='of the WhoIs lookup server')
+                        help='of the Whois lookup server')
     parser.add_argument('--port', default=PORT, type=int,
-                        help='Port of the WhoIs lookup server')
+                        help='Port of the Whois lookup server')
     parser.add_argument('-n', '--dry-run', default=False, action='store_true',
                         help='Only print the permutations')
     parser.add_argument('-o', '--only', default=False, action='store_true',
@@ -101,33 +143,56 @@ def parse_args(argv):
     parser.add_argument('--progress', default=False, action='store_true',
                         help='Show progress')
     parser.add_argument('domain',
-                        help='Domain to look up')
+                        help='Domain to look up. For example redCV.com')
 
     return parser.parse_args(argv)
 
+
+class Logger:
+    def __init__(self):
+        self.stderr_width = 0
+
+    def progress(self, string, *values):
+        value = string.format(*values) + '\r'
+        sys.stderr.write(value)
+        sys.stderr.flush()
+        self.stderr_width = len(value) - 1
+
+    def log(self, message, *values):
+        value = message.format(*values)
+        spaces = max(0, self.stderr_width - len(value))
+        print('{}{}'.format(value, ' ' * spaces))
+        self.stderr_width = 0
 
 def main(argv):
     args = parse_args(argv)
     host = args.host
     port = args.port
 
-    whois = WhoIs(host, port)
-    wildcard = Wildcard(args.domain)
+    logger = Logger()
+
+    domain = args.domain
+    if '.' not in domain:
+        domain += '.com'
+
+    whois = Whois(host, port)
+    wildcard = Wildcard(domain)
+    combinations = wildcard.combinations
 
     for i, domain in enumerate(wildcard.values):
         count = i + 1
-        if count % 100 == 0:
-            print('Count', count, file=sys.stderr)
+        # if count % 10 == 0:
+        logger.progress('Count: {} of {}', count, combinations)
 
         if args.dry_run:
-            print(domain)
+            logger.log(domain)
             continue
 
         registered  = whois.lookup(domain)
         if not registered:
-            print('✓', domain)
+            logger.log('✓ {}', domain)
         elif not args.only:
-            print('- ', domain)
+            logger.log('- {}', domain)
 
 
 if __name__ == '__main__':
