2572d40505fb1496cfdd90dab7a36e464d30bee0
Jerko Steiner <jerko.steiner@nyumc.org>
2018-11-29 00:26:30 +0100

    Add KeyboardInterrupt handler

--- whois.py
+++ whois.py
@@ -234,4 +234,8 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        print('\nProcess Interrupted')
+        exit(1)
