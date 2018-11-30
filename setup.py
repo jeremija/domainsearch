d6d3b3fe3f7458df7c83e42451ef499613a36afc
Jerko Steiner <jerko.steiner@nyumc.org>
2018-11-30 14:13:05 +0100

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='domainsearch',
-    version='1.0.0',
+    version='1.0.1',
     author='Jerko Steiner',
     author_email='jerko.steiner@gmail.com',
     description=(
